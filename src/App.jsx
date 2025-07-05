2c912b0676acbcf6990b29ed28319e3910bb84dc
sonsodockje <hanaroo0115@naver.com>
2025-07-05 17:47:40 +0900

    구조/ 라우터 따로 빼기

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
+
 import React from 'react';
 import './styles/App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './api/firebaseAuth';
-
-import MainPage from './pages/MainPage';
-import MyPage from './pages/MyPage';
-import DmPage from './pages/DmPage';
-import WritePage from './pages/WritePage';
-import Layout from './layout/Layout';
-import DetailPage from './pages/DetailPage';
-import NotFoundPage from './pages/NotFoundPage';
+import Router from './router';
 
 const queryClient = new QueryClient();
 
@@ -19,18 +13,7 @@ function App() {
         <BrowserRouter>
             <QueryClientProvider client={queryClient}>
                 <AuthProvider>
-                    <Routes>
-                        <Route element={<Layout />}>
-                            <Route index element={<MainPage />} />
-                            <Route path='account' element={<MyPage />} />
-                            <Route path='message' element={<DmPage />} />
-                            <Route path='detail/:id' element={<DetailPage />} />
-                            <Route path='write' element={<WritePage />} />
-                            <Route path='/write/:id' element={<WritePage />} />
-                            {/* <Route path="/posts/:id" element={<PostDetail />} /> */}
-                            <Route path='*' element={<NotFoundPage />} />
-                        </Route>
-                    </Routes>
+                    <Router />
                 </AuthProvider>
             </QueryClientProvider>
         </BrowserRouter>
@@ -38,3 +21,4 @@ function App() {
 }
 
 export default App;
+
